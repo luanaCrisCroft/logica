5749a917ff0c2005bd785ef43a186fbe99434f79
Luana Cristina <luanacrystinne@gmail.com>
2026-09-09 23:17:14 +0000

    adicionando meu código

--- input.js
+++ input.js
@@ -1,1 +1,18 @@
+const escrever = require("readline-sync")
+let nome = escrever.question("Qual seu nome?")
+var comidaPreferida = escrever.question("Qual sua comida preferida?")
+var idade =  parseInt(escrever.question("Qual é a sua idade?"))
+console.log(idade)
+if(idade >=25){
+    console.log("Pode entrar na festa, ebaaaaaaa")
+}else{
+    console.log("Volta pra casa, sorry")
+}
 
+if(idade >=25 && comidaPreferida == "Pizza"){
+    console.log("Pode entrar")
+}else if(idade >=25 && comidaPreferida =="Lasanha"){
+    console.log("converse com a dona da festa")
+}else{
+    console.log("volte amanhã porque hoje é pizzaaaaaa")
+}
